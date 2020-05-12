46c6e8d2fa17866834d278e9291e99b7bc0b5c64
Luan Vasconcelos <38038168+L4nche@users.noreply.github.com>
2020-05-12 16:35:06 -0300

    Add files via upload

---
+++ Aula_11/ex008.js
@@ -0,0 +1,7 @@
+// console.log('O console funciona!')
+var velocidade = 39
+console.log(`A velocidade do teu carango é de ${velocidade}Km/h`)
+if (velocidade > 40) {
+    console.log(`Você está acima da velocidade permitida em uma via residencial. MULTADO!`)
+}
+console.log(`Usa cinto aí mané`)
---
+++ Aula_11/ex009.js
@@ -0,0 +1,6 @@
+var anonascimento = 2005
+if (2020 - anonascimento < 18 ) {
+    console.log("Você é menor de idade")
+} else {
+    console.log(`Você é maior de idade`)
+}
