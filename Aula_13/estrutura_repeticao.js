f1a880eb3474d5efba2383deca5c1555a4ec3a9f
Luan Vasconcelos <38038168+L4nche@users.noreply.github.com>
2020-05-12 16:41:12 -0300

    Add files via upload

---
+++ Aula_13/estrutura_repeticao.js
@@ -0,0 +1,25 @@
+console.log('Vai começar....')
+for (luan = 21;  luan <= 30 ;luan++ ) {
+      console.log(`Luan, você tem ${luan} anos de idade, ainda da!`)
+}
+console.log('FIM!')
+
+//--------------------------- do while
+
+/*
+var cd = 2
+do {
+    console.log(`você tem ${cd} cd's`)
+    cd++
+} while (cd <= 10)
+
+
+
+//-------------------------------- while
+
+var cm = 1
+while (cm <= 16) {
+    console.log(`Voce tem ${cm}cm`)
+    cm++
+} 
+*/
